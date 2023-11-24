6897105fb540b1daf7e2c32eaf4442f0bda04c02
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-11-24 20:02:51 +0330

    add config module for env and run and connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: process.env?.DB_PORT ? +process.env?.DB_PORT : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.POSTGRES_PASSWORD + '',
+      database: process.env.POSTGRES_DB, 
+      synchronize: false,
+      logging: false,
+      entities: ['./entities/**/*.ts'],
+      migrations: ['./migrations/**/*.ts'],
+      subscribers: [],
+      migrationsTableName: 'migrations', 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
