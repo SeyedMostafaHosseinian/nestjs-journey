86b4b6f6fe02c3142020f939ad3d07a7a39c3c21
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2024-10-28 08:55:48 +0330

    some refactors in  env and db connections

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -6,12 +6,12 @@ dotenv.config();
 
 export const dataSourceConfig = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env?.DB_HOST || 'localhost',
   port: process.env?.DB_PORT ? +process.env?.DB_PORT : 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.POSTGRES_PASSWORD + '',
-  database: process.env.POSTGRES_DB,
-  synchronize: false,
+  username: process.env?.DB_USERNAME,
+  password: process.env?.DB_PASSWORD + '',
+  database: process.env?.DB_NAME,
+  synchronize: process.env?.DB_SYNC,
   logging: false,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
